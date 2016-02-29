179cd13841eb8f1ff21a35d61f34e41f74917f90
oaqque <oaqque@me.com>
2016-02-29 13:05:09 +1100

    heroku prep

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -6,7 +6,6 @@ class ResultsController < ApplicationController
   def by_year_and_month
     @bets_by_year = Bet.where("EXTRACT(YEAR FROM created_at) = ?", params[:year])
     @bets_by_month = @bets_by_year.where("EXTRACT(MONTH FROM created_at) = ?", params[:month])
-    @bets_by_month = @bets_by_month.all.group_by { |bet| bet.created_at.beginning_of_month }
     @bets_by_month.order("created_at")
   end
 end
