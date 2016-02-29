996a03b20cda6d6be1d2a08c24d38efe869dde16
oaqque <oaqque@me.com>
2016-02-29 12:33:26 +1100

    Results layout changed

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,5 +7,6 @@ class ResultsController < ApplicationController
     @bets_by_year = Bet.where("strftime('%Y', created_at) = ?", params[:year])
     @bets_by_month = @bets_by_year.where("strftime('%m', created_at) = ?", params[:month])
     @bets_by_month = @bets_by_month.all.group_by { |bet| bet.created_at.beginning_of_month }
+
   end
 end
