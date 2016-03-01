6e78ed4fbbc40e0e6291110e85410187abc65d60
oaqque <oaqque@me.com>
2016-03-01 19:41:06 +1100

    Added contacts table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229080827) do
+ActiveRecord::Schema.define(version: 20160301083849) do
 
   create_table "bets", force: :cascade do |t|
     t.string   "game"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20160229080827) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "bet_placed"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "plans", force: :cascade do |t|
