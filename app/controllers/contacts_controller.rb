622c62742f8988cb5268c4cf1ea6d9a38fce1ca2
oaqque <oaqque@me.com>
2016-03-02 23:45:35 +1100

    Fixed Problems with Contact Form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,19 @@
 class ContactsController < ApplicationController
-
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      name = params[:contact][:name]
-      email = params[:contact][:email]
-      body = params[:contact][:comments]
-      ContactMailer.contact_email(name, email, body).deliver
+      ContactMailer.contact_email(@contact.name, @contact.email, @contact.body).deliver
       flash[:success] = 'Message sent.'
-      redirect_to ""
+      redirect_to :back
     else
-      flash[:success] = 'Error occured, message has not been sent.'
-      redirect_to ""
+      flash[:success] = 'Error occurred, message has not been sent.'
+      redirect_to :back
     end
   end
 
   private
+
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
-
 end
