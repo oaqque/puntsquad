b7c468deeb3812f69dd2fb898bae890466a4c345
oaqque <oaqque@me.com>
2016-03-02 23:58:07 +1100

    Fixed no body method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,13 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+
+      name = params[:contact][:name]
+      email = params[:contact][:name]
+      body = params[:contact][:comments]
+      
       ContactMailer.contact_email(@contact.name, @contact.email, @contact.body).deliver
+
       flash[:success] = 'Message sent.'
       redirect_to :back
     else
