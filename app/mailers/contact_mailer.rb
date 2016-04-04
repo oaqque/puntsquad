d032962475b6c7b69ec51ade71b19ba39211bd9a
oaqque <oaqque@me.com>
2016-04-04 21:27:10 +1000

    Updated Contact Mailer Form and Contact Email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'oaqque@me.com'
+  default to: 'contact@puntsquad.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
