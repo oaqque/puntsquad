ab6ac512fd98cee79db67df38fdbe58b0f4ddac1
oaqque <oaqque@me.com>
2016-10-13 01:03:37 +1100

    Added keys for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,9 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => ENV['stripe_publishable_key'],
+  :secret_key      => ENV['stripe_api_key']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLISHABLE_KEY"]
+Stripe.api_key = ENV["stripe_api_key"]
+STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
