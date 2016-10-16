88f284d80a46cc89e0dc814681717e42c91a3f49
oaqque <oaqque@me.com>
2016-10-16 12:57:30 +1100

    Correctly handle card declined errors

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -30,34 +30,64 @@ class SubscriptionsController < ApplicationController
     token = params[:stripeToken]
 
     # Create a Stripe Customer
+    begin
+      customer = if current_user.stripeid?
+        Stripe::Customer.retrieve(current_user.stripeid)
+      else
+        Stripe::Customer.create(email: current_user.email)
+      end
 
-    customer = if current_user.stripeid?
-      Stripe::Customer.retrieve(current_user.stripeid)
-    else
-      Stripe::Customer.create(email: current_user.email)
+      subscription = customer.subscriptions.create(
+        source: params[:stripeToken],
+        plan: plan_id
+      )
+
+      options = {
+        stripeid: customer.id,
+        stripe_subscription_id: subscription.id
+      }
+
+      options.merge!(
+        card_last4: params[:card_last4],
+        card_exp_month: params[:card_exp_month],
+        card_exp_year: params[:card_exp_year],
+        card_type: params[:card_brand]
+      ) if params[:card_last4]
+
+      current_user.update(options)
+
+      redirect_to root_path
+      flash[:success] = "Payment Succeeded."
+    rescue Stripe::CardError => e
+      redirect_to new_subscription_path(:plan => plan_id)
+      flash[:danger] = 'Card Declined. You have not been charged. Please try again or contact support.'
+    rescue Stripe::RateLimitError => e
+      # Too many requests made to the API too quickly
+      redirect_to new_subscription_path(:plan => plan_id)
+      flash[:danger] = 'Rate Limit Error. You have not been charged. Please wait and try again later.'
+    rescue Stripe::InvalidRequestError => e
+      # Invalid parameters were supplied to Stripe's API
+      redirect_to new_subscription_path(:plan => plan_id)
+      flash[:danger] = 'Invalid Request Error. You have not been charged. Please try again or contact support.'
+    rescue Stripe::AuthenticationError => e
+      # Authentication with Stripe's API failed
+      # (maybe you changed API keys recently)
+      redirect_to new_subscription_path(:plan => plan_id)
+      flash[:danger] = 'Stripe API Declined. Please contact support.'
+    rescue Stripe::APIConnectionError => e
+      # Network communication with Stripe failed
+      redirect_to new_subscription_path(:plan => plan_id)
+      flash[:danger] = 'Network Connection Error. Please contact support.'
+    rescue Stripe::StripeError => e
+      # Display a very generic error to the user, and maybe send
+      # yourself an email
+      redirect_to new_subscription_path(:plan => plan_id)
+      flash[:danger] = 'Stripe Error. Please contact support.'
+    rescue => e
+      # Something else happened, completely unrelated to Stripe
+      redirect_to new_subscription_path(:plan => plan_id)
+      flash[:danger] = 'Unknown Error.'
     end
-
-    subscription = customer.subscriptions.create(
-      source: params[:stripeToken],
-      plan: plan_id
-    )
-
-    options = {
-      stripeid: customer.id,
-      stripe_subscription_id: subscription.id
-    }
-
-    options.merge!(
-      card_last4: params[:card_last4],
-      card_exp_month: params[:card_exp_month],
-      card_exp_year: params[:card_exp_year],
-      card_type: params[:card_brand]
-    ) if params[:card_last4]
-
-    current_user.update(options)
-
-    redirect_to root_path
-    flash[:success] = "Payment Succeeded."
   end
 
   def destroy
