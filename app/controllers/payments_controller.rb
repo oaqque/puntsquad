f8e3881975cc6b7c3f44949f55751c3b8769fecb
oaqque <oaqque@me.com>
2017-07-17 16:06:56 +1000

    Added comments for all paypal variables sent to PayPal

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,24 +14,32 @@ class PaymentsController < ApplicationController
   def paypal_url(return_path, plan_id)
     plan = Plan.find(plan_id)
     values = {
-        business: "merchant-success@puntsquad.com",
-        no_shipping: 0,
-        return: "#{Rails.application.secrets.app_host}#{return_path}",
-        rm: 2,
-        notify_url: "#{Rails.application.secrets.app_host}/hook",
-        item_name: plan.name,
-        currency_code: "AUD",
+        business: "merchant-success@puntsquad.com", # Your PayPal ID
+        no_shipping: 1,                             # Do not prompt for an address
+        return: "http://puntsquad.com",             # Return path after successful payment
+        cancel_return: "http://puntsquad.com",                          # Return path after cancelled payment
+        rm: 2,                                      # Post Method and all payment variables included
+        notify_url: "#{Rails.application.secrets.app_host}/hook", #
+        item_name: plan.name,                       # Description of item_name
+        currency_code: "AUD",                       # Currency Code
     }
     values = if plan.recurring
                 values.merge(
-                    cmd: "_xclick-subscriptions",
-                    a3: plan.price,
-                    p3: plan.duration,
-                    t3: plan.period.first,
-                    no_note: 1
+                    cmd: "_xclick-subscriptions",   # The button that the person clicked was a Subscribe button
+                    a3: plan.price,                 # Price of the Plan per instalment
+                    p3: plan.duration,              # Duration of subscription specified in T3
+                    t3: plan.period.first,          # Week/Month/Year
+                    src: 1,                         # Subscriptions payment recur 1.
+                    no_note: 1                      # Required, always set to 1 for subscribe button
                 )
               end
 
     "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+  end
+
+  def success
+  end
+
+  def cancel
   end
 end
